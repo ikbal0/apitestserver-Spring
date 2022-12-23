3b9d221eea6e71836ad391e9ab4509895b716987
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-12-23 13:11:25 +0700

    add post

--- src/main/java/com/apitestjava/apitestserver/Controller/ApiControllers.java
+++ src/main/java/com/apitestjava/apitestserver/Controller/ApiControllers.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apitestjava.apitestserver.Models.User;
@@ -24,4 +28,28 @@ public class ApiControllers {
     public List<User> getUser() {
         return userRepo.findAll();
     }
+
+    @PostMapping(value = "/user")
+    public String saveUser(User user) {
+        userRepo.save(user);
+        return "Saved..";
+    }
+
+    @PostMapping(value = "/save")
+    public @ResponseBody String addNewUser (
+        @RequestParam String firstName,
+        @RequestParam String lastName, 
+        @RequestParam int age,
+        @RequestParam String occupation) {
+    // @ResponseBody means the returned String is the response, not a view name
+    // @RequestParam means it is a parameter from the GET or POST request
+
+    User n = new User();
+    n.setFirstName(firstName);
+    n.setLastName(lastName);
+    n.setAge(age);
+    n.setOccupation(occupation);
+    userRepo.save(n);
+    return "Saved";
+  }
 }
